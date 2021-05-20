40856aebeb90554988be8f2fea0cf1bda3cf3cce
edup94 <eduardo.paredes@estudiantes.utec.edu.uy>
2021-05-20 19:05:20 +0000

    arregladas relaciones

--- entities/User.ts
+++ entities/User.ts
@@ -1,24 +1,20 @@
-import {
-    Entity, Column, PrimaryGeneratedColumn, ManyToMany, 
-    BaseEntity, JoinTable
-  } from 'typeorm';
-import { Favorite } from './Favorite';
-  
+import {Entity, Column, PrimaryGeneratedColumn,BaseEntity} from 'typeorm';
+
 @Entity()
-export class User extends BaseEntity{
-    
+export class User extends BaseEntity {
+
     @PrimaryGeneratedColumn()
     UserID: number;
-  
+
     @Column()
     Name: string;
-  
+
     @Column()
     LastName: string;
-  
-    @Column({unique: true})
+
+    @Column({ unique: true })
     Email: string;
-  
-    @Column({unique: true})
+
+    @Column({ unique: true })
     Password: string;
-  }
+}
